41ee178af844a6bb0e388affce88fc85986c284d
awilkins2 <125494114+awilkins2@users.noreply.github.com>
2023-09-13 14:28:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Names: Israel, Aaron
+
+#############################complete slide 1 challenge from week 4 slideshow##########
+#############################20 minutes################################################
+name = input("what is your name? ")
+commission = float(input("how much have you sold this month? "))
+percent = 100 / commission * 13
+print(
+f"Hello, {name}! You've sold {commission}. You're entitled to {percent} dollars.")
